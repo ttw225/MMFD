cc4c932d2fd0c35c09d06f3730c0e9647b9d36ab
TTW <s104321072@mail1.ncnu.edu.tw>
2018-12-21 03:06:29 +0800

    加入防呆功能
    try catch

--- master.py
+++ master.py
@@ -16,14 +16,18 @@ else:
     # 處理多個檔案
     for i in range(1, len(sys.argv)):
         print()
-        print("Reading File: {0:s} ... ".format(sys.argv[i]), end="")
-        data = pd.read_csv(sys.argv[i], sep="\t",
-                           index_col='Wavelength_Ref (nm)')
-        print("OK!")
-        print("Generating Image ... ", end="")
-        data['Quantum Efficiency (%)'].plot(title=data.columns[4])
-        print("OK!")
-        plt.show()
+        try:
+            print("Reading File: {0:s} ... ".format(sys.argv[i]), end="")
+            # 圖片要支援多一些功能
+            data = pd.read_csv(sys.argv[i], sep="\t",
+                               index_col='Wavelength_Ref (nm)')
+            print("OK!")
+            print("Generating Image ... ", end="")
+            data['Quantum Efficiency (%)'].plot(title=data.columns[4])
+            print("OK!")
+            plt.show()
+        except:
+            print("FALSE!!")
 
 print("Good Bye~")
 os.system('pause')
